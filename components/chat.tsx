1055e92554f4b29b91723c2a77c5ce63bd191885
Rumit Tuladhar <rumitt@live.com>
2023-11-20 18:50:05 +1030

    clean up

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,16 +1,10 @@
 import { Metadata } from 'next';
-import Image from 'next/image';
 import { CounterClockwiseClockIcon } from '@radix-ui/react-icons';
 
 import { Button } from '@/components/ui/button';
-import {
-	HoverCard,
-	HoverCardContent,
-	HoverCardTrigger,
-} from '@/components/ui/hover-card';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tabs, TabsContent } from '@/components/ui/tabs';
 import { Textarea } from '@/components/ui/textarea';
 
 import { CodeViewer } from '@/components/code-viewer';
