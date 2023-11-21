84ca638c531c72e8d062eea3697206c55e504877
Rumit Tuladhar <rumitt@live.com>
2023-11-21 14:39:09 +1030

    added chat prompt

--- components/chat.tsx
+++ components/chat.tsx
@@ -30,7 +30,9 @@ export default function ChatPage() {
 		<>
 			<div className='hidden h-full flex-col md:flex'>
 				<div className='container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16'>
-					<h2 className='text-lg font-semibold'>Playground</h2>
+					<h2 className='text-lg font-semibold whitespace-nowrap'>
+						Prompt Playground
+					</h2>
 					<div className='ml-auto flex w-full space-x-2 sm:justify-end'>
 						<PresetSelector presets={presets} />
 						<PresetSave />
