9c13ac26dc8af5fc542c8706afd5cff5c06af300
Rumit Tuladhar <rumitt@live.com>
2024-03-18 15:59:02 +1030

    Refactor Supabase server client and callback route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,18 +3,16 @@ import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
 export async function GET(request: Request) {
-	const requestUrl = new URL(request.url);
-	const code = requestUrl.searchParams.get('code');
-	const next = requestUrl.searchParams.get('next') ?? '/empty';
+	const { searchParams, origin } = new URL(request.url);
+	const code = searchParams.get('code');
+	const next = searchParams.get('next') ?? '/';
 
 	if (code) {
 		const cookieStore = cookies(); // Update the type of cookieStore to string
 		const supabase = createClient(); // Pass the cookieStore to the createClient function
 		const { error } = await supabase.auth.exchangeCodeForSession(code);
 		if (!error) {
-			return NextResponse.redirect(
-				new URL(`/${next.slice(1)}`, request.url)
-			);
+			return NextResponse.redirect(`${origin}${next}`);
 		}
 	}
 	// return the user to an error page with instructions
