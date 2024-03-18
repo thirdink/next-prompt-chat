bff527047adfcd1e248ddee062fe768857c6ae8a
Rumit Tuladhar <rumitt@live.com>
2024-03-18 16:43:32 +1030

    Add redirect to dashboard if user is logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import TopBar from '@/components/header/top-bar';
-
+import { redirect } from 'next/navigation';
+import getUserSession from '@/lib/hooks/getUserSession';
 import { GridBackground } from '@/components/grid-background';
 
 export default async function Index() {
+	const {
+		data: { session },
+	} = await getUserSession();
+
+	if (session) {
+		return redirect('/dashboard');
+	}
 	return (
 		<div className='flex-col md:flex'>
 			<div className='space-y-0.5'>
