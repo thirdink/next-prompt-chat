a520a0cb8312c8480b3a4fdee8a697222d45a782
Rumit Tuladhar <rumitt@live.com>
2024-04-13 18:41:16 +0930

    Update login button position in app/prompt/page.tsx

--- app/prompt/page.tsx
+++ app/prompt/page.tsx
@@ -1,17 +1,17 @@
-//TODO::
 // api to prompt table and check if publish is true ✅
 // if true  send prompt to client ✅
 // render prompt on client ✅
 // format the UI so it matches the dashboard/prompt selectListDisplay ✅
-// add login button
+// add login button to the top bar ✅
 import react from 'react';
+import Footer from '@/components/footer';
+import TopBar from '@/components/header/top-bar';
 import { promptService } from '@/service/client/prompt-service';
 import { SelectedListDisplay } from '@/components/selected-list-display';
 import { selectedChat } from '@/lib/types/chat/chat-lib';
 import { PromptProps } from '@/lib/types/prompt/prompt-lib';
 import { PostgrestError } from '@supabase/supabase-js';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import Footer from '@/components/footer';
 
 const Page = async ({
 	searchParams,
@@ -24,14 +24,26 @@ const Page = async ({
 
 	return (
 		<TooltipProvider delayDuration={0}>
-			<h1> Public Prompt Page</h1>
-			<p> {searchParams.promptId}</p>
-			{selectedChat && (
-				<>
-					<SelectedListDisplay item={selectedChat} />
-				</>
-			)}
-			<Footer />
+			<div className='flex-col md:flex h-[100vh]'>
+				<div className='space-y-0.5'>
+					<div className='text-muted-foreground'>
+						<TopBar />
+					</div>
+				</div>
+
+				{selectedChat !== null ? (
+					<>
+						<SelectedListDisplay item={selectedChat} />
+					</>
+				) : (
+					<div className='flex flex-row container'>
+						<div className='animate-in flex-1 flex flex-col gap-20 px-3 items-center container'>
+							<h2> This Prompt is not made public</h2>
+						</div>
+					</div>
+				)}
+				<Footer />
+			</div>
 		</TooltipProvider>
 	);
 };
